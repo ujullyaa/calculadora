23bd6e048070193bbeb22ebfa78cf0581ed690d5
jullya estelita da conceicao <jullyaestelita1958@gmail.com>
2025-07-18 11:49:43 -0300

    Desenvolvimento de uma calculadora simples

---
+++ calculadora.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const display = document.getElementById('display');
+const numeros = document.querySelectorAll('[id*=tecla]');
+const operadores = document.querySelectorAll('[id*=operador]');
+
+let novoNumero = true;
+let operador;
+let numeroanterior;
+
+const operacaoPendente = () => operador !== undefined;
+
+const calcular = () => {
+    if (operacaoPendente()) {
+        const numeroAtual = parseFloat(display.textContent.replace('.','').replace(',','.'));
+        novoNumero = true;
+        const resultado = eval('${numeroAnterior}${operador}${numeroAtual');
+        atualizarDisplay(resultado);
+    }
+};
+
+const atualizarDisplay = (texto) => {
+    if (novoNumero) {
+        display.textContent = texto.toLocaleString('BR');
+        novoNumero = false;
+    } else {
+        display.textContent += texto.toLocaleString('BR');
+    }
+    document.querySelector('#igual').focus();
+};
+
+const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent);
+numeroanterior.forEach((numero) => numero.addEventListener('click', ))
